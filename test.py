3072ce690b584a40ff6e919ba38cefdf5b89515d
YuzukiMIMURA <yuzuki725.m@gmail.com>
2018-12-07 17:21:46 +0900

    add select Source feature

--- test.py
+++ test.py
@@ -23,27 +23,36 @@ def frame_sub(img1, img2, img3, th):
 
 
 def main():
-    # カメラのキャプチャ
-    #cap = cv2.VideoCapture(0)
-    #動画読み込み 動画の名前
-    cap = cv2.VideoCapture("2.mov")
+    print("select Source")
+    select = input("0:from camera\n1:from video\n>> ")
+
+    if(select == "0"):
+        cap = cv2.VideoCapture(0)  # カメラのキャプチャ
     
+    if(select == "1"):
+        p = input("enter video path\n>> ")
+        cap = cv2.VideoCapture(p) #動画読み込み 動画の名前
+    else:
+        print("input is illiegal")
 
     try:
         # フレームを3枚取得してグレースケール変換
         frame1 = cv2.cvtColor(cap.read()[1], cv2.COLOR_RGB2GRAY)
         frame2 = cv2.cvtColor(cap.read()[1], cv2.COLOR_RGB2GRAY)
         frame3 = cv2.cvtColor(cap.read()[1], cv2.COLOR_RGB2GRAY)
+        h, w = frame1.shape
+        plt.plot(w,-1*h,marker='.')
     except:
         _ = 0
     
-    h, w = frame1.shape
-    plt.plot(w,-1*h,marker='.')
+    
+   
 
     #前回の座標値を記憶する変数
     before_x = 0
     before_y = 0
 
+    cnt = 0
     while(cap.isOpened()):
         # フレーム間差分を計算
         mask = frame_sub(frame1, frame2, frame3, th=10)
@@ -58,26 +67,26 @@ def main():
         """
         #print(area)
 
-        #面積が閾値より大きければ、重心の座標を更新
-        if area > 600:
+        
+        if area > 400: #面積が閾値より大きければ、重心の座標を更新
             mu = cv2.moments(mask, False)
             try:
                 x,y = int(mu["m10"]/mu["m00"]), int(mu["m01"]/mu["m00"])
-                plt.plot(x,-1*y,marker='.')
-                plt.plot([before_x,x],[-1*before_y,-1*y])
+                plt.plot(x,-1*y,'r',marker='.',markersize=3)
+                plt.plot([before_x,x],[-1*before_y,-1*y],'g',linewidth = 0.5)
                 before_x = x
                 before_y = y
                 cv2.circle(mask, (x,y), 8, (255,255,255),-1)
-            except:
-                print(" ")
 
-        #面積が閾値より小さければ、前回の座標を表示
-        else :
+            except:
+                _ = 0
+
+        else :   #面積が閾値より小さければ、前回の座標を表示
             cv2.circle(mask, (before_x,before_y), 8, (255,255,255),-1)
 
 
         # 結果を表示
-        cv2.imshow("Frame2", frame2)
+        #cv2.imshow("Frame2", frame2)
         cv2.imshow("Mask", mask)
 
         try:
@@ -92,6 +101,7 @@ def main():
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+        cnt = cnt+1
     cap.release()
     cv2.destroyAllWindows()
     plt.show()
