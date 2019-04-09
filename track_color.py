6ee454c1bb304b404410f6c022038d77ea6b2c6b
ken54kama <kengo.bmx@gmail.com>
2019-04-09 12:15:46 +0900

    colortracking

---
+++ track_color.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+
+def red_detect(img):
+    # HSV色空間に変換
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # 赤色のHSVの値域1
+    hsv_min = np.array([0,0,0])
+    hsv_max = np.array([330,45,29])
+    mask1 = cv2.inRange(hsv, hsv_min, hsv_max)
+
+    # 赤色のHSVの値域2
+    hsv_min = np.array([150,127,0])
+    hsv_max = np.array([179,255,255])
+    mask2 = cv2.inRange(hsv, hsv_min, hsv_max)
+
+    return mask1 + mask2
+
+
+def main():
+    # カメラのキャプチャ
+    cap = cv2.VideoCapture("wire.mov")
+
+    while(cap.isOpened()):
+        # フレームを取得
+        ret, frame = cap.read()
+
+        # 赤色検出
+        mask = red_detect(frame)
+
+        # 結果表示
+        cv2.imshow("Frame", frame)
+        cv2.imshow("Mask", mask)
+
+        # qキーが押されたら途中終了
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
