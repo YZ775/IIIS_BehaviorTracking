480f2713a9d3f8d3db1812ace406a2fce1b4275a
Yuzuki Mimura <yuzuki725.m@gmail.com>
2019-07-25 19:26:13 +0900

    add arduino connection

--- camtest.py
+++ camtest.py
@@ -1,27 +1,27 @@
+import numpy as np
 import cv2
-import time
- 
+
 cap = cv2.VideoCapture(1)
- 
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-cap.set(cv2.CAP_PROP_FPS, 30)           # カメラFPSを60FPSに設定
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640) # カメラ画像の横幅を1280に設定
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480) # カメラ画像の縦幅を720に設定
-while True:
- 
-    ret,frame = cap.read()
- 
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
- 
-    cv2.imshow('frame',frame)
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('output.mp4',fourcc, 20.0, (640,480))
 
-    start = time.time()
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret==True:
+        frame = cv2.flip(frame,0)
+
+        # write the flipped frame
+        out.write(frame)
+
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
         break
-    process_time = time.time() - start
-    print(process_time)
- 
+
+# Release everything if job is finished
 cap.release()
+out.release()
 cv2.destroyAllWindows()
--- track_color.py
+++ track_color.py
@@ -43,7 +43,7 @@ class mouseParam:
         return (self.mouseEvent["x"], self.mouseEvent["y"])
         
 
-def CreateMask(img,centor_hsv):
+def CreateMask(img,centor_hsv,centor_hsv2):
     # HSV色空間に変換
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
@@ -54,18 +54,23 @@ def CreateMask(img,centor_hsv):
     hsv_max = centor_hsv + np.array([20,120,30])
     mask1 = cv2.inRange(hsv, hsv_min, hsv_max)
 
-    """
+    
     # 赤色のHSVの値域2
-    hsv_min = np.array([150,127,0])
-    hsv_max = np.array([179,255,255])
-    mask2 = cv2.inRange(hsv, hsv_min, hsv_max)
-    """
+    hsv_min2 = centor_hsv2 - np.array([20,50,30])
+    hsv_max2 = centor_hsv2 + np.array([20,120,30])
+    mask2 = cv2.inRange(hsv, hsv_min2, hsv_max2)
+    
     kernel = np.ones((5,5),np.uint8)
-    return cv2.medianBlur(cv2.dilate(mask1,kernel,iterations = 1),15)
+    return cv2.medianBlur(cv2.dilate(cv2.erode(mask1+mask2,kernel,iterations = 1),kernel,iterations = 1),15)
     #return mask1
 
 
 def main():
+    print("select Source")
+    select = input("0:from camera else: PATH\n")
+    print("\n")
+    if(select == "0" or select == "1"):
+        select = int(select)
     #####変数初期化#####
     x = 0
     y = 0
@@ -74,36 +79,62 @@ def main():
     distance = np.zeros(10)
 
     centor_hsv = np.array([0,0,0])
-    cap = cv2.VideoCapture("wire.mov")
+    centor_hsv2 = np.array([0,0,0])
+
+    cap = cv2.VideoCapture(select)
     ret, frame = cap.read()
     
-    mask_1 = CreateMask(frame,centor_hsv)
+    mask_1 = CreateMask(frame,centor_hsv,centor_hsv2)
     mask_2 = mask_1
     mask_3 = mask_1
+
+    print(frame.shape[0])
+    print(frame.shape[1])
+
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter('output.mp4',fourcc, 20.0, (2*frame.shape[1],frame.shape[0]))
     ###############
-    ser = serial.Serial('/dev/cu.usbmodem143203', 9600,parity=serial.PARITY_NONE)
+    ser = serial.Serial('/dev/tty.usbserial-14330', 9600,parity=serial.PARITY_NONE)
     print(ser.name)
     #マウスクリックで対象を選択
-    
+
+    cv2.namedWindow('Frame')
     frame_temp = frame.copy()
     cv2.imshow("Frame", frame_temp)
     mouseData = mouseParam("Frame")
 
+    flag1 = 0
+    flag2 = 0
     while(1):
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        
+        
         if mouseData.getEvent() == cv2.EVENT_LBUTTONDOWN:
-            frame_temp = frame.copy()
-            print(mouseData.getPos())
-            hsv = cv2.cvtColor(frame_temp,cv2.COLOR_BGR2HSV)
-            centor_hsv = hsv[mouseData.getY(),mouseData.getX(),:]
-            cv2.circle(frame_temp, (mouseData.getX(),mouseData.getY()), 7, (255,0,0),-1)
-            cv2.imshow("Frame", frame_temp)
-            print(hsv[mouseData.getY(),mouseData.getX(),:])
-            
-        elif mouseData.getEvent() == cv2.EVENT_RBUTTONDOWN:
+            if(flag1 ==0):
+                #frame_temp = frame.copy()
+                print(mouseData.getPos())
+                hsv = cv2.cvtColor(frame_temp,cv2.COLOR_BGR2HSV)
+                centor_hsv = hsv[mouseData.getY(),mouseData.getX(),:]
+                cv2.circle(frame_temp, (mouseData.getX(),mouseData.getY()), 7, (255,0,0),-1)
+                cv2.imshow("Frame", frame_temp)
+                print(centor_hsv)
+                flag1 = 1
+        
+        if mouseData.getEvent() == cv2.EVENT_RBUTTONDOWN:
+            if(flag2 ==0):
+                #frame_temp = frame.copy()
+                print(mouseData.getPos())
+                hsv = cv2.cvtColor(frame_temp,cv2.COLOR_BGR2HSV)
+                centor_hsv2 = hsv[mouseData.getY(),mouseData.getX(),:]
+                cv2.circle(frame_temp, (mouseData.getX(),mouseData.getY()), 7, (255,255,0),-1)
+                cv2.imshow("Frame", frame_temp)
+                print(centor_hsv2)
+                flag2 = 1
+         
+        """elif mouseData.getEvent() == cv2.EVENT_RBUTTONDOWN:
             break
-    
+        """
     cv2.destroyAllWindows()
 
     while(cap.isOpened()):
@@ -112,7 +143,7 @@ def main():
 
         mask_1 = mask_2
         mask_2 = mask_3
-        mask_3 = CreateMask(frame,centor_hsv)
+        mask_3 = CreateMask(frame,centor_hsv,centor_hsv2)
         """
         # フレームの絶対差分
         diff1 = cv2.absdiff(mask_1, mask_2)
@@ -136,21 +167,35 @@ def main():
                 _ = 0
         distance = np.roll(distance, 1) #要素数10の配列をシフトしてsumを取ることで矩形波とのconvolution
         distance[0] = np.sqrt((x-before_x)**2+(y-before_y)**2)
-        print(distance.sum())
-        ser.write(str(distance.sum())) 
+
+        distance_str = str(distance.sum())
+        print(distance_str)
+        ser.write(distance_str.encode('utf-8')) 
         ser.write(b'\n')
         ser.reset_output_buffer()
 
         show_frame = cv2.cvtColor(mask_3,cv2.COLOR_GRAY2RGB)
         cv2.circle(show_frame, (x,y), 7, (0,0,255),-1)
+
+        frame_copy = frame.copy()
+        cv2.rectangle(frame_copy, (0,0), (400,50), (0,0,0), thickness=-1, lineType=cv2.LINE_8, shift=0)
+        cv2.putText(frame_copy, distance_str, (40, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), thickness=2)
  
-        cv2.imshow("Mask", cv2.hconcat([show_frame, frame]))
+        out_frame = cv2.hconcat([show_frame,frame_copy])
+        cv2.imshow("Mask",out_frame)
+
+        out.write(out_frame)
+
+
+
         # qキーが押されたら途中終了
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     cap.release()
     cv2.destroyAllWindows()
+    ser.close()
+    out.release()
 
 
 if __name__ == '__main__':
