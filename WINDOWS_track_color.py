5406c4ab2d9325e79204851ed3b5134ae102b1da
ZEROdcFRAME <kengo.bmx@gmail.com>
2019-08-02 10:54:29 +0900

    no change

--- WINDOWS_track_color.py
+++ WINDOWS_track_color.py
@@ -11,7 +11,7 @@ def select_port():
     ser.timeout = 0.1       # タイムアウトの時間
 
     ports = list_ports.comports()    # ポートデータを取得
-    
+
     devices = [info.device for info in ports]
 
     if len(devices) == 0:
@@ -28,7 +28,7 @@ def select_port():
         print("input number of target port >> ",end="")
         num = int(input())
         ser.port = devices[num]
-    
+
     # 開いてみる
     try:
         ser.open()
@@ -44,39 +44,39 @@ class mouseParam:
         self.mouseEvent = {"x":None, "y":None, "event":None, "flags":None}
         #マウス入力の設定
         cv2.setMouseCallback(input_img_name, self.__CallBackFunc, None)
-    
+
     #コールバック関数
     def __CallBackFunc(self, eventType, x, y, flags, userdata):
-        
+
         self.mouseEvent["x"] = x
         self.mouseEvent["y"] = y
-        self.mouseEvent["event"] = eventType    
-        self.mouseEvent["flags"] = flags    
+        self.mouseEvent["event"] = eventType
+        self.mouseEvent["flags"] = flags
 
     #マウス入力用のパラメータを返すための関数
     def getData(self):
         return self.mouseEvent
-    
+
     #マウスイベントを返す関数
     def getEvent(self):
-        return self.mouseEvent["event"]                
+        return self.mouseEvent["event"]
 
     #マウスフラグを返す関数
     def getFlags(self):
-        return self.mouseEvent["flags"]                
+        return self.mouseEvent["flags"]
 
     #xの座標を返す関数
     def getX(self):
-        return self.mouseEvent["x"]  
+        return self.mouseEvent["x"]
 
     #yの座標を返す関数
     def getY(self):
-        return self.mouseEvent["y"]  
+        return self.mouseEvent["y"]
 
     #xとyの座標を返す関数
     def getPos(self):
         return (self.mouseEvent["x"], self.mouseEvent["y"])
-        
+
 
 def CreateMask(img,centor_hsv,centor_hsv2):
     # HSV色空間に変換
@@ -89,12 +89,12 @@ def CreateMask(img,centor_hsv,centor_hsv2):
     hsv_max = centor_hsv + np.array([20,120,30])
     mask1 = cv2.inRange(hsv, hsv_min, hsv_max)
 
-    
+
     # 赤色のHSVの値域2
     hsv_min2 = centor_hsv2 - np.array([20,50,30])
     hsv_max2 = centor_hsv2 + np.array([20,120,30])
     mask2 = cv2.inRange(hsv, hsv_min2, hsv_max2)
-    
+
     kernel = np.ones((5,5),np.uint8)
     return cv2.medianBlur(cv2.dilate(cv2.erode(mask1+mask2,kernel,iterations = 1),kernel,iterations = 1),15)
     #return mask1
@@ -118,7 +118,7 @@ def main():
 
     cap = cv2.VideoCapture(select)
     ret, frame = cap.read()
-    
+
     mask_1 = CreateMask(frame,centor_hsv,centor_hsv2)
     mask_2 = mask_1
     mask_3 = mask_1
@@ -130,7 +130,7 @@ def main():
 
     fourcc = cv2.VideoWriter_fourcc(*'DIVX')
     out = cv2.VideoWriter('output.avi',fourcc, 20.0, (2*CAP_W,CAP_H))
-    
+
     ########serialのopen#######
     print("Select Device")
     ser = select_port()
@@ -149,8 +149,8 @@ def main():
     while(1):
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-        
-        
+
+
         if mouseData.getEvent() == cv2.EVENT_LBUTTONDOWN:
             if(flag1 ==0):
                 #frame_temp = frame.copy()
@@ -161,7 +161,7 @@ def main():
                 cv2.imshow("Frame", frame_temp)
                 print(centor_hsv)
                 flag1 = 1
-        
+
         if mouseData.getEvent() == cv2.EVENT_RBUTTONDOWN:
             if(flag2 ==0):
                 #frame_temp = frame.copy()
@@ -172,7 +172,7 @@ def main():
                 cv2.imshow("Frame", frame_temp)
                 print(centor_hsv2)
                 flag2 = 1
-         
+
     cv2.destroyAllWindows()
 
     while(cap.isOpened()):
@@ -184,7 +184,7 @@ def main():
 
         area = cv2.countNonZero(mask_3)
 
-        
+
         before_x = x
         before_y = y
         if(area > 5000):
@@ -209,7 +209,7 @@ def main():
         frame_copy = frame.copy()
         cv2.rectangle(frame_copy, (0,0), (400,50), (0,0,0), thickness=-1, lineType=cv2.LINE_8, shift=0)
         cv2.putText(frame_copy, distance_str, (40, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), thickness=2)
- 
+
         out_frame = cv2.hconcat([show_frame,frame_copy])
         cv2.imshow("Mask",out_frame)
 
